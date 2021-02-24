ac3a31f3bfb35c3d36e04dde6606579f745f167a
Jens Sproede <j.sproede@nanogiants.de>
2021-02-24 14:47:04 +0100

    refactor(sonarcloud): fix code smells

--- src/lib/utils/example.util.ts
+++ src/lib/utils/example.util.ts
@@ -13,8 +13,7 @@ export const merge = (examples: Examples, newExamples: Examples) => {
         const indexOfNo = val.lastIndexOf(SEPARATOR);
 
         if (indexOfNo >= 0) {
-          const number = parseInt(val.substring(indexOfNo + SEPARATOR.length), 10);
-          return number;
+          return parseInt(val.substring(indexOfNo + SEPARATOR.length), 10);
         }
 
         return acc;
